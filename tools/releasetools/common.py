8af5579b9968d8ef9be284571275e4c854496a05
Ameya Thakur <ameyat@codeaurora.org>
2013-07-29 17:39:37 -0700

    build: Add changes to release tools and mkbootimg
    Change the prototype of LoadRecoveryFstab to take in the device type
    as a argument. Fix the case where mkbootimg was being passed a incorrect
    argument.
    
    Change-Id: Ic6ac596d8d96d3a5effbdf513763ec1cb92b1a03

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -230,6 +230,9 @@ def LoadInfoDict(input_file, input_dir=None):
             vendor_base_fs_file,)
         del d["vendor_base_fs_file"]
 
+
+  if "device_type" not in d:
+    d["device_type"] = "MMC"
   try:
     data = read_helper("META/imagesizes.txt")
     for line in data.split("\n"):
@@ -263,14 +266,7 @@ def LoadInfoDict(input_file, input_dir=None):
   if d.get("no_recovery", None) != "true":
     recovery_fstab_path = "RECOVERY/RAMDISK/etc/recovery.fstab"
     d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"],
-        recovery_fstab_path, system_root_image)
-  elif d.get("recovery_as_boot", None) == "true":
-    recovery_fstab_path = "BOOT/RAMDISK/etc/recovery.fstab"
-    d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"],
-        recovery_fstab_path, system_root_image)
-  else:
-    d["fstab"] = None
-
+                                   d.get("system_root_image", d["device_type"], False))
   d["build.prop"] = LoadBuildProp(read_helper)
   return d
 
@@ -293,8 +289,7 @@ def LoadDictionaryFromLines(lines):
       d[name] = value
   return d
 
-def LoadRecoveryFSTab(read_helper, fstab_version, recovery_fstab_path,
-                      system_root_image=False):
+def LoadRecoveryFSTab(read_helper, fstab_version, type, system_root_image=False):
   class Partition(object):
     def __init__(self, mount_point, fs_type, device, length, device2, context):
       self.mount_point = mount_point
@@ -478,6 +473,11 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
     fn = os.path.join(sourcedir, "tagsaddr")
     if os.access(fn, os.F_OK):
       cmd.append("--tags-addr")
+      cmd.append(open(fn).read().rstrip("\n"))
+
+    fn = os.path.join(sourcedir, "tags_offset")
+    if os.access(fn, os.F_OK):
+      cmd.append("--tags_offset")
       cmd.append(open(fn).read().rstrip("\n"))
 
     fn = os.path.join(sourcedir, "ramdisk_offset")
