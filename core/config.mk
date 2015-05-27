ee6aa262b934d681730b783614c646e3defe1c46
Adnan Begovic <adnan@cyngn.com>
2015-05-26 18:21:15 -0700

    build/core: Inherit vendor/* makefiles for check-api.
    Change-Id: I72d409075192b18c35068566c5687092d45be9b4

--- core/config.mk
+++ core/config.mk
@@ -889,4 +889,10 @@ endif
 # Nitrogen common sepolicy
 include $(TOPDIR)vendor/nitrogen/sepolicy/sepolicy.mk
 
+# Include any vendor specific config.mk file
+-include $(TOPDIR)vendor/*/build/core/config.mk
+
+# Include any vendor specific apicheck.mk file
+-include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
