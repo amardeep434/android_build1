6b069d837fce2bd54653ea371b2503bb9b51f375
Alex <mazdarider23@gmail.com>
2016-08-25 23:05:57 -0400

    Remove some moar packages
    Change-Id: Ibfb163c11befb244328ed305393bdb9d1bac5483

--- target/product/core.mk
+++ target/product/core.mk
@@ -32,8 +32,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
